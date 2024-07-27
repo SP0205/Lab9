080069b9eb53735479ecf2bc23adf39bd9b26ca0
SP0205 <siyao.pei0205@gmail.com>
2024-07-26 21:19:44 -0400

    encode function have cloned

--- Lab9_software.py
+++ Lab9_software.py
@@ -5,6 +5,11 @@ def decode(password):
         decoded_password += decoded_char
     return decoded_password
 
+def encode(password):
+    encoded_list = []
+    for i in range(len(password)):
+        encoded_list.append(int(password[i]) + 3)
+    return ''.join(str(j) for j in encoded_list)
 
 def main():
     encoded_password = ''
